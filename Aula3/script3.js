ea9d44cfc39735edfcec141f813e02df3a321e34
wesleiads <101604299+wesleiads@users.noreply.github.com>
2022-03-31 19:42:20 -0300

    new files

--- Aula3/script3.js
+++ Aula3/script3.js
@@ -1,10 +1,9 @@
     function create() {
     let a = document.createElement('p');
-    a.textContent=y;
+    a.textContent="12345678910";
     document.body.appendChild(a);
 }
-   
     const button= document.querySelectorAll('#botao3');
-    for(var y= 0; y < 10 ; y++){
+    for(var y= 0; y < button.length ; y++) {//alterado indice de i para X
     button[y].addEventListener('click', create);
 }
