513e4746544233ba449cf9e49c42c86f49b5cb73
wesleiads <101604299+wesleiads@users.noreply.github.com>
2022-04-28 03:01:44 -0300

    changes

---
+++ Projeto Escola/AreaPais/layouts/js/frequencia.js
@@ -0,0 +1,6 @@
+function pesquisa() {
+
+    var selopera = window.document.getElementById('selectmes');
+  
+    var operador = selectmes.value; //Obtendo o valor do <option> selecionado
+    if (operador == 'Janeiro')
---
+++ Projeto Escola/login/js/login.js
@@ -0,0 +1,4 @@
+function acessar() {
+    var usuario = window.document.getElementById('login');
+    var pass = window.document.getElementById('senha');
+  
